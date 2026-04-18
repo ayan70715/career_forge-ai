d4efffab877e1efe69a633551ad8df7693deb218
Ayan Jana <161872771+ayan70715@users.noreply.github.com>
2026-04-18 17:27:14 +0530

    Update three-jsx.d.ts

--- three-jsx.d.ts
+++ three-jsx.d.ts
@@ -1,5 +1,13 @@
+import * as THREE from 'three'
 import { ThreeElements } from '@react-three/fiber'
 
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
+// This is a safety fallback for some build environments
 declare global {
   namespace JSX {
     interface IntrinsicElements extends ThreeElements {}
