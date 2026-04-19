923f70fc9b4a3f52e232d53e85983fd2f08b1b3f
Ayan Jana <161872771+ayan70715@users.noreply.github.com>
2026-04-19 23:13:44 +0530

    Update InterviewRoomClient.tsx

--- src/app/interview-prep/room/InterviewRoomClient.tsx
+++ src/app/interview-prep/room/InterviewRoomClient.tsx
@@ -198,7 +198,7 @@ function AvatarTile({
       {/* Camera config proven to show face/bust correctly */}
       <Canvas
         shadows
-        camera={{ position: [0, 0.1, 0.9], fov: 20 }}
+        camera={{ position: [0, 1.6, 1.2], fov: 24 }}
         gl={{ antialias: true, alpha: true }}
         style={{ height: "100%", width: "100%", background: "transparent" }}
       >
